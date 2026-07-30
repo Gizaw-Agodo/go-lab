73bfb3757a88df144af4cca450e0fc85fb2d9839
Gizaw <gizawag123@gmail.com>
2026-07-30 16:48:06 +0300

    add git ignore for env

---
+++ 09-library-api/internal/database/postgress.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"go-lab/09-library-api/internal/config"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func New(cfg *config.Config )(*sql.DB, error) {
+	db,err := sql.Open("pgx",cfg.DatabaseURL )
+
+	if err != nil {
+		return nil, fmt.Errorf("open database : %w", err)
+	}
+
+	cxt, cancel := context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(cxt); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("ping db : %w", err)
+	}
+
+	// connection poll config 
+	db.SetMaxOpenConns(cfg.DBMaxOpenConns)
+	db.SetMaxIdleConns(cfg.DBMaxIdleConns)
+	db.SetConnMaxLifetime(time.Duration(cfg.DBConnMaxLifetime) * time.Minute)
+	db.SetConnMaxIdleTime(time.Duration(cfg.DBConnMaxIdleTime) * time.Minute)
+
+	return db, nil 
+}
+
