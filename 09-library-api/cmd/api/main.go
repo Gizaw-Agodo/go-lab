afed862d37b514103c295fd8aa74ce21c3e2ec98
Gizaw <gizawag123@gmail.com>
2026-07-30 16:45:17 +0300

    integrate config

--- 09-library-api/cmd/api/main.go
+++ 09-library-api/cmd/api/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"go-lab/09-library-api/internal/config"
+	"go-lab/09-library-api/internal/database"
 	"go-lab/09-library-api/internal/handlers"
+	"go-lab/09-library-api/internal/repositories"
 	"go-lab/09-library-api/internal/server"
+	"go-lab/09-library-api/internal/services"
 	"log"
 )
 
 func main() {
+	cfg := config.Load()
+	database,err := database.New(cfg)
+	if err != nil {
+		log.Fatalf("failed to initialize database: %v", err)
+	}
+
+	bookRepo := repositories.NewPostgressBookRepository(database)
+	bookService := services.Newbookservice(bookRepo)
+
 	homeHandler := handlers.NewHomeHandler()
-	bookHandler := handlers.NewBookHandler()
+	bookHandler := handlers.NewBookHandler(bookService)
 
 	srv := server.New(homeHandler, bookHandler)
 	
